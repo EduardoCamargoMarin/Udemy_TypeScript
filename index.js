f929e5ea7daeb6b56992ce1507bd19da66af1a66
Eduardo <du.marin10@gmail.com>
2023-01-10 18:00:15 -0300

    Udemy_Aulas

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-//-----------------------------------------EX 01-------------------------------------------------------------------
-// console.log('h...') pedimos para o compiler a partir do tsc <nome do arquivo.ts> para mapear para um .js
-//-----------------------------------------EX 02-------------------------------------------------------------------
-var a = 10; // quando você define a variavel o TS vai sempre avisar você sobre qual o tipo da variavel, ele não é dinamico como o JS
-var b = 'Eduardo';
+function add(num1, num2) {
+    return num1 + num2;
+}
+var result = add(4, 8);
+console.log(result);
--- index.ts
+++ index.ts
@@ -1,8 +1,6 @@
-//-----------------------------------------EX 01-------------------------------------------------------------------
+function add(num1: number, num2: number) {
+    return num1+num2
+}
 
-// console.log('h...') pedimos para o compiler a partir do tsc <nome do arquivo.ts> para mapear para um .js
-
-//-----------------------------------------EX 02-------------------------------------------------------------------
-
-let a = 10 // quando você define a variavel o TS vai sempre avisar você sobre qual o tipo da variavel, ele não é dinamico como o JS
-let b= 'Eduardo'
+let result = add(4,8)
+console.log(result)
