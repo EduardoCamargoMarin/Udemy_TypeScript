3af1fa4cffc601b29d1c7fadd90d7ec13e12173c
Eduardo <du.marin10@gmail.com>
2023-01-10 17:05:04 -0300

    Udemy_Aulas

---
+++ index.js
@@ -0,0 +1,5 @@
+//-----------------------------------------EX 01-------------------------------------------------------------------
+// console.log('h...') pedimos para o compiler a partir do tsc <nome do arquivo.ts> para mapear para um .js
+//-----------------------------------------EX 02-------------------------------------------------------------------
+var a = 10; // quando você define a variavel o TS vai sempre avisar você sobre qual o tipo da variavel, ele não é dinamico como o JS
+var b = 'Eduardo';
---
+++ index.ts
@@ -0,0 +1,8 @@
+//-----------------------------------------EX 01-------------------------------------------------------------------
+
+// console.log('h...') pedimos para o compiler a partir do tsc <nome do arquivo.ts> para mapear para um .js
+
+//-----------------------------------------EX 02-------------------------------------------------------------------
+
+let a = 10 // quando você define a variavel o TS vai sempre avisar você sobre qual o tipo da variavel, ele não é dinamico como o JS
+let b= 'Eduardo'
